1ecf7b4585f967170380213faf245cc85cd94e6f
Ilya Timofeev <il.tmfv@gmail.com>
2018-12-13 17:51:21 +0300

    Fix formatting

--- src/transit_decoder/core.cljs
+++ src/transit_decoder/core.cljs
@@ -1,12 +1,12 @@
 (ns transit-decoder.core
-    (:require [reagent.core :as r]
-              [clojure.pprint :refer [pprint]]
-              [cognitect.transit :as transit]))
+  (:require [reagent.core :as r]
+            [clojure.pprint :refer [pprint]]
+            [cognitect.transit :as transit]))
 
 (enable-console-print!)
 
 (defonce app-state (r/atom {:transit ""
-                          :clojure ""}))
+                            :clojure ""}))
 
 (defonce transit-str* (r/cursor app-state [:transit]))
 (defonce clojure-str* (r/cursor app-state [:clojure]))
@@ -15,21 +15,21 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
 
 (defn convert []
   (let [converted-data (transit/read (transit/reader :json) @transit-str*)
         pretty-converted-data (with-out-str (pprint converted-data))]
     (reset! clojure-str* (str pretty-converted-data))))
-  
+
 (defn app []
   [:div
-    [:h3 "Transit"]
-    [:textarea {:value @transit-str* 
-                :on-change #(reset! transit-str* (.. % -target -value))}]
-    [:h3 "Clojure"]
-    [:textarea {:value @clojure-str*
-                :on-click #(.preventDeafult %)}]
-    [:button {:on-click convert} "Convert Transit -> Clojure"]])
+   [:h3 "Transit"]
+   [:textarea {:value @transit-str*
+               :on-change #(reset! transit-str* (.. % -target -value))}]
+   [:h3 "Clojure"]
+   [:textarea {:value @clojure-str*
+               :on-click #(.preventDeafult %)}]
+   [:button {:on-click convert} "Convert Transit -> Clojure"]])
 
 (r/render [app] (.getElementById js/document "app"))
